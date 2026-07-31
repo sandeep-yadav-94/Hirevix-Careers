f2e5aff124c83a00e740b9af02cc18c89a21e0b1
sandeep-yadav <sandeepyadavtheking@gmail.com>
2026-07-30 19:30:41 -0700

    job service completed

--- services/job/src/controllers/job.ts
+++ services/job/src/controllers/job.ts
@@ -89,3 +89,179 @@ export const createCompany = TryCatch(async(req:AuthenticatedRequest, res) =>{
     })
 
 })
+
+export const deleteCompany = TryCatch(async(req:AuthenticatedRequest, res) =>{
+
+    const user = req.user;
+    const {companyId} = req.params;
+
+    const [company] = await sql`SELECT logo_public_id FROM companies WHERE company_id = ${companyId} AND recruiter_id = ${user?.user_id}`;
+
+    if(!company){
+        throw new ErrorHandler(404, "Company not found or you are not authorized to delete it");
+    }
+
+    await sql`DELETE FROM companies WHERE company_id = ${companyId}`;
+
+    res.json({
+        message: "Company and all associated jobs have been deleted",
+    })
+
+})
+
+
+export const createJob = TryCatch(async(req:AuthenticatedRequest, res) => {
+
+    const user = req.user;
+
+    if(!user){
+        throw new ErrorHandler(401, "Authentication required");
+    }
+
+    if(user.role != "recruiter"){
+        throw new ErrorHandler(403, "Forbidden: Only recruiter can creeate a job");
+    }
+
+    const {title, description, salary, location, role, job_type, work_location, company_id, openings} = req.body;
+    
+    if(!title || !description || !salary || !location || !role || !openings){
+        throw new ErrorHandler(400, "All the fields are required");
+    }
+
+    const [company] = await sql`SELECT company_id FROM companies WHERE company_id = ${company_id} AND recruiter_id = ${user.user_id}`;
+
+    if(!company){
+        throw new ErrorHandler(404, "Company not found");
+    }
+
+    const [newJob] = await sql`INSERT INTO jobs (title, description, salary, location, role, job_type, work_location, company_id, posted_by_recruiter_id, openings) VALUES (${title}, ${description}, ${salary}, ${location}, ${role}, ${job_type}, ${work_location}, ${company_id}, ${user.user_id}, ${openings}) RETURNING *`;
+
+    res.json({
+        message: "Job Posted Successfully",
+        job: newJob,
+    })
+
+})
+
+
+export const updateJob = TryCatch(async(req:AuthenticatedRequest, res) =>{
+
+    const user = req.user;
+
+    if(!user){
+        throw new ErrorHandler(401, "Authentication required");
+    }
+
+    if(user.role != "recruiter"){
+        throw new ErrorHandler(403, "Forbidden: Only recruiter can creeate a job");
+    }
+
+    const {title, description, salary, location, role, job_type, work_location, company_id, openings, is_active} = req.body;
+
+    const [existingJob] = await sql`SELECT posted_by_recruiter_id FROM jobs WHERE job_id = ${req.params.jobId}`;
+
+    if(!existingJob){
+        throw new ErrorHandler(404, "Job not found");
+    }
+
+    if(existingJob.posted_by_recruiter_id !== user.user_id){
+        throw new ErrorHandler(403, "Forbidden: You are not allowed");
+    }
+
+    const [updatedJob] = await sql`UPDATE jobs SET title = ${title},
+    description = ${description},
+    salary = ${salary},
+    location = ${location},
+    role = ${role},
+    job_type = ${job_type},
+    work_location = ${work_location},
+    openings = ${openings},
+    is_active = ${is_active}
+    WHERE job_id = ${req.params.jobId} RETURNING *;
+    `;
+
+
+    res.json({
+        message:"Job updated successfully",
+        job : updatedJob,
+    })
+
+})
+
+
+
+export const getAllCompany = TryCatch(async(req:AuthenticatedRequest, res)=>{
+
+    const companies = await sql`SELECT * FROM companies WHERE recruiter_id = ${req.user?.user_id}`;
+
+    res.json(companies);
+
+})
+
+
+
+export const getCompanyDetails = TryCatch(async(req:AuthenticatedRequest, res) =>{
+    const {id} = req.params;
+
+    if(!id){
+        throw new ErrorHandler(400, "Company id is required");
+    }
+
+    const [companyData] = await sql`SELECT c.*, COALESCE(
+    (
+       SELECT json_agg(j.*) FROM jobs j WHERE j.company_id = c.company_id
+    ),
+    '[]'::json
+    ) AS jobs
+     FROM companies c WHERE c.company_id = ${id} GROUP BY c.company_id;`;
+
+    if(!companyData){
+        throw new ErrorHandler(404, "company not found");
+    }
+
+    res.json(companyData);
+})
+
+
+
+
+export const getAllActiveJobs = TryCatch(async (req, res) =>{
+
+    const { title, location } = req.query as {
+        title?: string;
+        location?: string;
+    };
+
+    let queryString = `SELECT j.job_id, j.title, j.description, j.salary, j.location, j.job_type, j.role, j.work_location, j.created_at, c.name AS company_name, c.logo AS company_logo, c.company_id AS company_id FROM jobs j JOIN companies c ON j.company_id = c.company_id WHERE j.is_active = true `;
+
+    const values = [];
+
+    let paramIndex = 1;
+
+    if(title){
+        queryString += ` AND j.title ILIKE $${paramIndex}`;
+        values.push(`%${title}%`);
+        paramIndex++;
+    }
+
+    if(location){
+        queryString += ` AND j.location ILIKE $${paramIndex}`;
+        values.push(`%${location}%`);
+        paramIndex++;
+    }
+
+    queryString += " ORDER BY j.created_at DESC";
+
+    const jobs = await sql.query(queryString, values) as any[];
+
+    res.json(jobs);
+})
+
+
+
+export const getSingleJob = TryCatch(async(req, res) => {
+    const job = await sql`SELECT * FROM jobs WHERE job_id = ${req.params.jobId}`;
+    res.json(job);
+})
+
+
--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import { isAuth } from '../middleware/auth.js';
 import uploadFile from '../middleware/multer.js';
-import { createCompany } from '../controllers/job.js';
+import { createCompany, createJob, deleteCompany, getAllActiveJobs, getAllCompany, getCompanyDetails, getSingleJob, updateJob } from '../controllers/job.js';
 const router = express.Router();
 
 router.post("/company/new", isAuth, uploadFile, createCompany);
+router.delete("/company/:companyId", isAuth, deleteCompany);
+router.post("/new", isAuth, createJob);
+router.put("/:jobId", isAuth, updateJob)
+router.get("/company/all", isAuth, getAllCompany);
+router.get("/company/:id", isAuth, getCompanyDetails);
+router.get("/all", getAllActiveJobs)
+router.get("/:jobId", getSingleJob);
 
 export default router;
