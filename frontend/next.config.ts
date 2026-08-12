ff6aebf3acfe2da8502643a2df4b78823cadb3fc
sandeep-yadav <sandeepyadavtheking@gmail.com>
2026-08-12 02:46:26 -0700

    ready for deployment frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,13 +4,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
-  
-  
-   
+  allowedDevOrigins: ['10.61.206.126'],
 };
 
 export default nextConfig;
-
-module.exports = {
-  allowedDevOrigins: ['10.61.206.126'],
-}
